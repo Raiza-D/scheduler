e07f6899ae314dd42dc8d9df488509d99ba30dc6
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-12 14:44:23 -0700

    Define formatSpots function and implement it. Add console log to test props.spots. Update h3 to invoke formatSpots function

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,6 +6,17 @@ import classNames from "classnames";
 
 export default function DayListItem(props) {
 
+  const formatSpots = () => {
+    if (props.spots === 0) {
+      return "no spots remaining"
+    }
+    if (props.spots === 1) {
+      return `${props.spots} spot remaining`
+    }
+    console.log("These are the spots: ", props.spots);
+    return `${props.spots} spots remaining`;
+  };
+
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": !props.spots,
@@ -14,7 +25,7 @@ export default function DayListItem(props) {
   return (
     <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
