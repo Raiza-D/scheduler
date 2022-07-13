71c0bc2d2cbde105e8f6a9d41980b984184667db
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-12 20:14:14 -0700

    Add stories for Storybook, taken from Compass. Stories for InterviewerListItem component

--- stories/index.js
+++ stories/index.js
@@ -8,6 +8,8 @@ import "index.scss";
 import Button from "components/Button";
 import DayListItem from "../src/components/DayListItem";
 import DayList from "../src/components/DayList";
+import InterviewerListItem from "../src/components/InterviewerListItem";
+
 
 storiesOf("Button", module)
   .addParameters({
@@ -70,3 +72,38 @@ storiesOf("DayList", module)
     <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
   ));
 
+
+// Stories for InterviwerListItem
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png",
+};
+
+storiesOf("InterviewerListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+  })
+  .add("Unselected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+    />
+  ))
+  .add("Selected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected
+    />
+  ))
+  .add("Clickable", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      setInterviewer={action("setInterviewer")}
+    />
+  ));
