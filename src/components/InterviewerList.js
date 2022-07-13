f0f71008222fe3ec720d8ffccf7c0ad660155f2a
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-13 11:30:26 -0700

    Declare variable containing array. Iterate over array to show each interviewer in the list. Pass in props. Return variable in the end inside <ul> tag

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,14 +1,29 @@
 import React from "react";
 
 import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
 
 
 export default function InterviewerList(props) {
 
+  const allInterviewers = props.interviewers.map((interviewer) => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.name === props.interviewer}
+        setInterviewer={props.setInterviewer}
+      />
+    )
+  })
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+        {allInterviewers}
+      </ul>
     </section>
   );
 }
