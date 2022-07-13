b82974d6b7b8ea2970f19deeaffa1c166cc7d0a3
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-12 22:16:25 -0700

    Edit Line 9 class name for base class styling. Should have included the __item. Fixes issue of cursor pointer not showing on hover on Storybook

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -6,7 +6,7 @@ import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
 
-  const interviewerClass = classNames("interviewers", {
+  const interviewerClass = classNames("interviewers__item", {
     "interviewers__item--selected": props.selected
   });
 
