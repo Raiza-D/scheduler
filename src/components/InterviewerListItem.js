40571b94e473f94d4cc829bf3b394d29d8dee117
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-12 20:31:52 -0700

    Replace hard-coded values with props. Add onClick event handler to <li> tag.

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -6,13 +6,13 @@ import "components/InterviewerListItem.scss";
 export default function InterviewerListItem(props) {
 
   return (
-    <li className="interviewers__item">
+    <li onClick={() => props.setInterviewer(props.name)} className="interviewers__item">
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
+      {props.name}
     </li>
   );
 }
