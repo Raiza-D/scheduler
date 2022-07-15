4fa423bbdf2ba4b81a8128dfed7d6ec3256fba05
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-14 17:43:01 -0700

    Import Button and InterviewerList components

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import Button from "../components/Button";
-import InterviewerList from "../components/InterviewerList";
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
 
