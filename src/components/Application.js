9545782471060a06293389500192f543dce47f2e
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-20 16:18:40 -0700

    Import getAppointmentsForDay function

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
+import { getAppointmentsForDay } from "helpers/selectors";
 
 
 export default function Application(props) {
