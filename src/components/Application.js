d563665e070ab4b5cb06e8946cceb1f9fea9a0a0
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-20 12:23:30 -0700

    Fix bug of page blanking when clicking on any of the days. Added the original first useState for day and setDay, with Monday as the initial state. Then changed Line 81 accordingly. This state takes care of switching days by clicking on each

--- src/components/Application.js
+++ src/components/Application.js
@@ -47,6 +47,7 @@ const appointments = {
 
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
   const [days, setDays] = useState([]);
 
   useEffect(() => {
@@ -77,7 +78,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={days} onChange={setDays} />
+          <DayList days={days} value={day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
