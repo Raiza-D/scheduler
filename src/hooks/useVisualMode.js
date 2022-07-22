b02fbae2b9e28c687836ff4f2915847995344f2d
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-22 15:26:19 -0700

    Uncomment code tracking the mode. Remove alternative method on Line 26 whereby we ensure mode passed into return obj is the mode on top of stack (the current one)

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,11 +1,11 @@
 import { React, useState } from "react";
 
 export default function useVisualMode(initial) {
-  // const [mode, setMode] = useState(initial);
+  const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
   function transition(mode) {
-    // setMode(mode);
+    setMode(mode);
 
     const newHistory = [...history];
     newHistory.push(mode);
@@ -20,10 +20,8 @@ export default function useVisualMode(initial) {
     const newHistory = [...history];
     newHistory.pop();
     setHistory(newHistory);
-    // setMode(newHistory[newHistory.length - 1]);
+    setMode(newHistory[newHistory.length - 1]);
   }
-
-  const mode = history[history.length - 1];
 
   return { mode, transition, back };
 }
