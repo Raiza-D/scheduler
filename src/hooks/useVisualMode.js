730e54eb8fe91b942e428987b089f7cc3ab59d23
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-22 17:02:11 -0700

    Write code to skip modes back to initial value. Re-write entire code to avoid bugs later so no longer using push and pop and now using callbacks. Will remove old code later

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,27 +1,39 @@
 import { React, useState } from "react";
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
+  // const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition(mode) {
-    setMode(mode);
+  function transition(mode, replace = false) {
+    // setMode(mode);
 
-    const newHistory = [...history];
-    newHistory.push(mode);
-    setHistory(newHistory);
+    // const newHistory = [...history];
+    // newHistory.push(mode);
+    
+    if (replace) {
+      // newHistory.pop();
+      // newHistory.push(mode);
+      setHistory(prev => [...prev.slice(0, -1), mode])
+    } else {
+      setHistory(prev => [...prev, mode]);
+    }
   }
 
   function back() {
-    if (history.length === 1) {
+    if (history.length <= 1) {
       return;
     }
 
-    const newHistory = [...history];
-    newHistory.pop();
-    setHistory(newHistory);
-    setMode(newHistory[newHistory.length - 1]);
+    setHistory((prev) => [...prev.slice(0, -1)]);
+
+    // const newHistory = [...history];
+    // newHistory.pop();
+    // setHistory(newHistory);
+    // setMode(newHistory[newHistory.length - 1]);
   }
 
-  return { mode, transition, back };
+
+  return { mode: history[history.length - 1], transition, back };
 }
+
+// Set React states using callbacks inside setState callbacks
