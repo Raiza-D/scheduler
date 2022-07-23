a7689bc518e65a6181505ae7ad3c5c5c302b43bd
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-22 17:32:11 -0700

    Import useVisualMode function. Add empty and show constants

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,7 +4,10 @@ import "./styles.scss"
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "../src/hooks/useVisualMode";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 export default function Appointment(props) {
   
