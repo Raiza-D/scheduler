c7dfac52cc7e89fd7c39ee20748c217bb80f68ee
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-22 17:06:26 -0700

    Declare mode var containing last mode value in history array

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,18 +1,10 @@
 import { React, useState } from "react";
 
 export default function useVisualMode(initial) {
-  // const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
   function transition(mode, replace = false) {
-    // setMode(mode);
-
-    // const newHistory = [...history];
-    // newHistory.push(mode);
-    
     if (replace) {
-      // newHistory.pop();
-      // newHistory.push(mode);
       setHistory(prev => [...prev.slice(0, -1), mode])
     } else {
       setHistory(prev => [...prev, mode]);
@@ -25,15 +17,10 @@ export default function useVisualMode(initial) {
     }
 
     setHistory((prev) => [...prev.slice(0, -1)]);
-
-    // const newHistory = [...history];
-    // newHistory.pop();
-    // setHistory(newHistory);
-    // setMode(newHistory[newHistory.length - 1]);
   }
 
+  const mode = history[history.length - 1];
 
-  return { mode: history[history.length - 1], transition, back };
+  return { mode, transition, back };
 }
 
-// Set React states using callbacks inside setState callbacks
