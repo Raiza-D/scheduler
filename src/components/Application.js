e2fdb01aaae78bc557fe957f1c211754d776b33f
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-26 13:14:17 -0700

    Update appointment object

--- src/components/Application.js
+++ src/components/Application.js
@@ -47,7 +47,18 @@ export default function Application(props) {
 
   function bookInterview(id, interview) {
     console.log(id, interview);
-  }
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    }
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    }
+
+  };
   
   const appointmentsList =
     dailyAppointments.map((appointment) => {
