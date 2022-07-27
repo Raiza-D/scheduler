e07b10912dc50dbcbaa3a60353c10e771f6cd9d6
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-27 11:28:17 -0700

    Set up deleting appts - define cancelInterview func with its logic. Copy each indiv appt, then all appts while updating given appt id. Update backend with delete operation for given appt id. Then set the state. Pass in cancelInterview as a ref to Appointment component

--- src/components/Application.js
+++ src/components/Application.js
@@ -67,7 +67,29 @@ export default function Application(props) {
         console.log(error.message);
       });
   };
-  
+
+  function cancelInterview(id) {
+    console.log(id);
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    }
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    }
+
+    return axios.delete(`/api/appointments/${id}`)
+      .then((response) => {
+        setState({...state, appointments});
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
+
   const appointmentsList =
     dailyAppointments.map((appointment) => {
       const interview = getInterview(state, appointment.interview);
@@ -79,6 +101,7 @@ export default function Application(props) {
             interview={interview}
             interviewers={interviewersList}
             bookInterview={bookInterview}
+            cancelInterview={cancelInterview}
           />
       );
     });
