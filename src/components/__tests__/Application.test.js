ee1e1bcf7a8f541ee81a4a0cd6943c5fe8cba024
Raiza De Guzman <santos_dg7@hotmail.com>
2022-08-06 14:09:44 -0700

    Add code for steps 4 to 8

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -93,7 +93,7 @@ describe("Application", () => {
 
   it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
     // 1. Render the Application.
-    const { container } = render(<Application />);
+    const { container, debug } = render(<Application />);
 
   // 2. Wait until the text "Archie Cohen" is displayed.
     await waitForElement(() => getByText(container, "Archie Cohen"));
@@ -104,17 +104,31 @@ describe("Application", () => {
     );
 
     fireEvent.click(getByAltText(appointment, "Edit"));
-
     
-  // 4. Check that the Save button is shown on the editable form.
-    expect(getByText(appointment, "Edit")).toBeInTheDocument();
+  // 4. Check that the Save button is shown.
+    expect(getByText(appointment, "Save")).toBeInTheDocument();
+
   // 5. Change the student name
+    fireEvent.change(
+        getByPlaceholderText(appointment, /Enter student name/i),
+        {
+          target: { value: "Alice Wonderland" },
+        }
+      );
   // 6. Click the "Save" button on the form.
+    fireEvent.click(getByText(appointment, "Save"));
+
   // 7. Check that the element with the text "Saving" is displayed.
+    expect(getByText(appointment, "SAVING")).toBeInTheDocument();
+
   // 8. Wait until the element with the "Edit" button is displayed.
+    await waitForElement(() => getByAltText(appointment, "Edit"));
+
   // 9. Check that the DayListItem with the text "Monday" still has the text "2 spots remaining".
-  
-  })
+
+  debug();
+
+  });
 
 });
 
