dcc50a7a544446c6f93d7472375472c646709b3a
Raiza De Guzman <santos_dg7@hotmail.com>
2022-08-06 12:48:15 -0700

    Edit plan to follow provided solution on Compass

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -66,11 +66,12 @@ describe("Application", () => {
   // 2. Wait until the text "Archie Cohen" is displayed.
   await waitForElement(() => getByText(container, "Archie Cohen"));
 
-  // 3. Click the "Delete" button on the Form.
-  // 4. Click "Delete" button on the confirmation.
-  // 5. Check that the element with the text saying "Deleting" is displayed.
-  // 6. Check that the empty appointment now shows the "Add" button.
-  // 7. Check that the DayListItem with the text "Monday" and its spots remaining has increased by 1
+  // 3. Click the "Delete" button on the booked appointment.
+  // 4. Check that the confirmation message is shown.
+  // 5. Click the "Confirm" button on the confirmation.
+  // 6. Check that the element with the text "Deleting" is displayed.
+  // 7. Wait until the element with the "Add" button is displayed.
+  // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
 
 
     });
