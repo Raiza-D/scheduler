e03e1cd9532564ead147b808188e3b2065fcd869
Raiza De Guzman <santos_dg7@hotmail.com>
2022-08-06 12:59:57 -0700

    Add async function to start of second test. Add code for step 3 of plan. Use debug function to check current state of DOM

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -30,7 +30,7 @@ describe("Application", () => {
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
     const { container, debug } = render(<Application />);
 
-    await waitForElement(() => getByText(container, "Archie Cohen"))
+    await waitForElement(() => getByText(container, "Archie Cohen"));
 
     const appointments = getAllByTestId(container, "appointment");
 
@@ -58,23 +58,29 @@ describe("Application", () => {
   });
 
   it(
-    "loads data, cancels an interview and increases the spots remaining for Monday by 1", () => {
+    "loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
 
       // 1. Render the Application.
-  const { container } = render(<Application />);
+  const { container, debug } = render(<Application />);
 
   // 2. Wait until the text "Archie Cohen" is displayed.
   await waitForElement(() => getByText(container, "Archie Cohen"));
 
   // 3. Click the "Delete" button on the booked appointment.
+  const appointment = getAllByTestId(container, "appointment").find(appointment => queryByText(appointment, "Archie Cohen"));
+
+  fireEvent.click(getByAltText(appointment, "Delete"));
+
   // 4. Check that the confirmation message is shown.
+  
   // 5. Click the "Confirm" button on the confirmation.
   // 6. Check that the element with the text "Deleting" is displayed.
   // 7. Wait until the element with the "Add" button is displayed.
   // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
-
-
-    });
+  
+  
+    debug();
+  });
 
 });
 
