75e6f6a96802279efb72a2f6d80dd1a6cbdf5311
Raiza De Guzman <santos_dg7@hotmail.com>
2022-08-06 14:20:23 -0700

    Remove debug function

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -93,7 +93,7 @@ describe("Application", () => {
 
   it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
     // 1. Render the Application.
-    const { container, debug } = render(<Application />);
+    const { container } = render(<Application />);
 
   // 2. Wait until the text "Archie Cohen" is displayed.
     await waitForElement(() => getByText(container, "Archie Cohen"));
@@ -130,9 +130,6 @@ describe("Application", () => {
     );
 
     expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
-
-  debug();
-
   });
 
 });
