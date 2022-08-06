7366500087565d0bd62eba178c474b9953f72106
Raiza De Guzman <santos_dg7@hotmail.com>
2022-08-06 13:25:36 -0700

    Add code for step 6 and 7

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -60,26 +60,30 @@ describe("Application", () => {
   it(
     "loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
 
-      // 1. Render the Application.
+    // 1. Render the Application.
     const { container, debug } = render(<Application />);
 
-  // 2. Wait until the text "Archie Cohen" is displayed.
+    // 2. Wait until the text "Archie Cohen" is displayed.
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
-  // 3. Click the "Delete" button on the booked appointment.
+    // 3. Click the "Delete" button on the booked appointment.
     const appointment = getAllByTestId(container, "appointment").find(appointment => queryByText(appointment, "Archie Cohen"));
 
     fireEvent.click(getByAltText(appointment, "Delete"));
 
-  // 4. Check that the confirmation message is shown.
+    // 4. Check that the confirmation message is shown.
     expect(getByText(appointment, "Are you sure you would like to delete?")).toBeInTheDocument();
 
-  // 5. Click the "Confirm" button on the confirmation.
+    // 5. Click the "Confirm" button on the confirmation.
     fireEvent.click(getByText(appointment, "Confirm"));
 
-  // 6. Check that the element with the text "Deleting" is displayed.
-  // 7. Wait until the element with the "Add" button is displayed.
-  // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+    // 6. Check that the element with the text "Deleting" is displayed.
+    expect(getByText(appointment, "DELETING")).toBeInTheDocument();
+
+    // 7. Wait until the element with the "Add" button is displayed.
+    await waitForElement(() => getByAltText(appointment, "Add"));
+
+    // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
   
   
     debug();
