0afe7452be89cab80f49743683c38d8596e98e40
Raiza De Guzman <santos_dg7@hotmail.com>
2022-08-09 14:05:05 -0700

    Add code to test that an interviewer is selected

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -12,6 +12,9 @@ describe("Appointments", () => {
 
     cy.get("[data-testid=student-name-input]")
       .type("Lydia Miller-Jones");
+
+    cy.get("[alt='Sylvia Palmer']")
+      .click();
   });
 
 });
