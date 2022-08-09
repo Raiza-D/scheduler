dbd00c1d13a6d1f8ceef3bcda00dd2240300216c
Raiza De Guzman <santos_dg7@hotmail.com>
2022-08-08 19:09:41 -0700

    Remove find function

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -6,6 +6,6 @@ describe("Navigation", () => {
   it("should navigate to Tuesday", () => {
     cy.visit("/");
 
-    cy.get("li").find("h2").contains("Tuesday").click()
+    cy.get("li").contains("Tuesday").click()
   })
 });
