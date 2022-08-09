98a95eafc91dc4e9d30f68c1cdb9843981088eba
Raiza De Guzman <santos_dg7@hotmail.com>
2022-08-09 13:58:32 -0700

    Add code to test student name being entered into input field

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -6,9 +6,12 @@ describe("Appointments", () => {
 
     cy.contains("li", "Monday") // What's the difference w/ only having Monday?
 
-    cy.get("[alt=Add]")
+    cy.get("[alt=Add]") // We use first() func bc we hide the second Add button. Where is the code for this?
       .first()
       .click();
+
+    cy.get("[data-testid=student-name-input]")
+      .type("Lydia Miller-Jones");
   });
 
 });
