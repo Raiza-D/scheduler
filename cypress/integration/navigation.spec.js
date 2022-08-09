ee790c35f4f294b3843416b0ff5cd079e0fa8b96
Raiza De Guzman <santos_dg7@hotmail.com>
2022-08-08 19:09:07 -0700

    Add second test to check if it navigates to Tuesday

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,4 +2,10 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+
+    cy.get("li").find("h2").contains("Tuesday").click()
+  })
 });
