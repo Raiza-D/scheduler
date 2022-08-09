094c9bbba42beb96db33d79aa5c9476e03c45a16
Raiza De Guzman <santos_dg7@hotmail.com>
2022-08-09 13:50:58 -0700

    Add second test to look for Add button and clicks it

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -3,8 +3,12 @@ describe("Appointments", () => {
 
   it("should book an interview", () => {
     cy.visit("/");
-    
-    cy.contains("li", "Monday")
-  })
+
+    cy.contains("li", "Monday") // What's the difference w/ only having Monday?
+
+    cy.get("[alt=Add]")
+      .first()
+      .click();
+  });
 
 });
