50d07deadd0ca1672b652ac21665717f45aa0e76
Raiza De Guzman <santos_dg7@hotmail.com>
2022-08-08 19:21:15 -0700

    Add assertion to check that element has certain CSS properties

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -6,6 +6,11 @@ describe("Navigation", () => {
   it("should navigate to Tuesday", () => {
     cy.visit("/");
 
-    cy.get("li").contains("Tuesday").click()
-  })
+    cy.get("li")
+      .contains("Tuesday")
+      .click();
+    
+    cy.contains("li", "Tuesday")
+      .should("have.css", "background-color", "rgb(242, 242, 242)");
+  });
 });
