91c28858d92f35cc32eaf72cb27c63d6c6f7e9dd
Raiza De Guzman <santos_dg7@hotmail.com>
2022-08-10 14:48:00 -0700

    Remove spacing between imports top of file

--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -1,11 +1,8 @@
 import React from "react";
-
 import { render, cleanup, fireEvent } from "@testing-library/react";
-
 import Button from "components/Button";
 
 afterEach(cleanup);
-
 
 it("renders its `children` prop as text", () => {
   const { getByText } = render(<Button>Default</Button>);
