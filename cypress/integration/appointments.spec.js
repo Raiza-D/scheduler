44b71fb3759eea919daa2cbe3ad71bc7c3e2d9b2
Raiza De Guzman <santos_dg7@hotmail.com>
2022-08-10 14:58:12 -0700

    Remove my commented notes and questions. Edit comment top of file

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,4 +1,4 @@
-// Appointment tests
+// Tests to ensure correct behaviour for when appts are booked, edited, and cancelled
 describe("Appointments", () => {
 
   beforeEach(() => {
@@ -6,27 +6,26 @@ describe("Appointments", () => {
 
     cy.visit("/");
     
-    cy.contains("li", "Monday") // What's the difference w/ only having Monday?
+    cy.contains("Monday")
   });
 
   it("should book an interview", () => {
-    cy.get("[alt=Add]") // We use first() func bc we hide the second Add button. Where is the code for this?
+    cy.get("[alt=Add]")
       .first()
       .click();
 
     cy.get("[data-testid=student-name-input]")
       .type("Lydia Miller-Jones");
 
-    cy.get("[alt='Sylvia Palmer']") // Why is Sylvia Palmer in single quotes?
+    cy.get("[alt='Sylvia Palmer']")
       .click();
 
     cy.contains("Save")
       .click();
 
     cy.contains(".appointment__card--show", "Lydia Miller-Jones")
-    cy.contains(".appointment__card--show", "Sylvia Palmer"); // This is checking Sylvia Palmer for the first appointment. How do I check for Sylvia Palmer in the second and CORRECT appointment?
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
-
 
   it("should edit an interview", () => {
     cy.get("[alt=Edit]")
@@ -47,7 +46,6 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Tori Malcolm");
   });
 
-
   it("should cancel an interview", () => {
     cy.get("[alt=Delete]")
       .click({ force: true });
@@ -62,5 +60,4 @@ describe("Appointments", () => {
       .should("not.exist");
   
   });
-
 });
